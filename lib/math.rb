971c179f26edf82488fb1de476c109867d24141d
Jackson Gray Morgan <jacksonmo@me.com>
2020-01-03 00:45:43 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  sum 
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
+  difference
 end
 
 def division(num1, num2)
-
+  quotient = num1 / num2
+  quotient
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  product
 end
 
 def modulo(num1, num2)
-
+  quotient = num1 % num2
+  quotient
 end
 
 def square_root(num)
-
+  num**2
+  square_root(num)
 end
